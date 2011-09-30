d87780cbbbe06e4f2f0dcc31b84f8c191a28e5b3
Joseph Freeman <jfree143dev@gmail.com>
2011-09-29 21:55:04 -0400

    Solved problem 1 with ruby

---
+++ src/0001/solution.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+#PROBLEM 1 (05 October 2001):
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+#get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+class Array
+    def sum
+        self.inject{|sum,x| sum + x }
+    end
+end
+
+def method1
+    ans = 0
+    (3...1000).each do |n|
+        if n % 3 == 0 or n % 5 == 0
+            ans += n
+        end
+    end
+    puts "Method 1: " + ans.to_s
+end
+
+def method2
+    ans = ((3...1000).step(3).to_a + (5...1000).step(5).to_a).uniq.sum
+    puts "Method 3: " + ans.to_s
+end
+
+method1
+method2
