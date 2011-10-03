dce957cafd65ece77f3e49e181c07559aeebe561
Joseph Freeman <jfree143dev@gmail.com>
2011-10-02 20:40:44 -0400

    Solved problem 16 with python.

---
+++ src/0016/solution.py
@@ -0,0 +1,11 @@
+#!/bin/bash/env python
+
+#215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+
+def method1():
+    sm = sum(int(i) for i in str(2**1000))
+    print("Method 1: ", sm)
+
+if __name__ == '__main__':
+    method1()
